5b8a86260b3d700d35d4175f2601b1d5807e149f
Jess Hatfield <joshhatfield@qogita.com>
2024-11-18 19:25:54 +0000

    test:  added test cases for word length multiplier

--- app/message_cost_calculator/message_cost_calculator.py
+++ app/message_cost_calculator/message_cost_calculator.py
@@ -3,20 +3,32 @@ from decimal import Decimal
 from typing import List
 
 
-
-
 class CalculatorRule(ABC):
 
-    def calculate(self, message: str) -> Decimal:
+    def calculate(self, text: str) -> Decimal:
         pass
 
 
 class BaseCostRule(CalculatorRule):
-    def calculate(self, message: str) -> Decimal:
+    def calculate(self, text: str) -> Decimal:
         return Decimal(1)
 
 
+class CharacterCountRule(CalculatorRule):
+    def calculate(self, text: str) -> Decimal:
+        return Decimal(f'{len(text)}') * Decimal('0.05')
+
+class WordLengthMultiplierRule(CalculatorRule):
+    def calculate(self, text: str) -> Decimal:
+        return
+
 class MessageCostCalculator:
+    """
+    We use a strategy pattern here to keep cost calculation
+        -Extendable
+        -Easier to understand
+        -Easier to test
+    """
 
     def __init__(self, calculators: List[CalculatorRule]):
         self.__calculator_rules = calculators
--- app/tests/test_message_cost_calculator.py
+++ app/tests/test_message_cost_calculator.py
@@ -2,7 +2,8 @@ from decimal import Decimal
 
 import pytest
 
-from app.message_cost_calculator.message_cost_calculator import MessageCostCalculator, BaseCostRule
+from app.message_cost_calculator.message_cost_calculator import MessageCostCalculator, BaseCostRule, CharacterCountRule, \
+    WordLengthMultiplierRule
 
 
 def test_message_cost_calculator_applies_rules():
@@ -10,3 +11,21 @@ def test_message_cost_calculator_applies_rules():
 
     calculator = MessageCostCalculator(calculators=[BaseCostRule()])
     assert calculator.calculate_cost(text=text) == Decimal(1)
+
+
+def test_character_count_rule():
+    text = 'my hovercraft is full of eels'
+
+    assert CharacterCountRule().calculate(text=text) == Decimal('1.45')
+
+
+@pytest.mark.parametrize('text,expected_cost', [
+    ('cat bat', Decimal('0.2')),
+    ('jeff steve jeremy jessica', Decimal('0.8')),
+     ('Octopus phlebotomist', Decimal('0.8')),
+     ('cat@ stev- octopus', Decimal('0.6'))]
+                         )
+def test_word_length_multipler_rule(text, expected_cost):
+    text = 'my hovercraft is full of eels'
+
+    assert WordLengthMultiplierRule().calculate(text=text) == expected_cost
